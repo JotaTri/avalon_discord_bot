bcff90ba4983396dde6d0ce5c1ae92358bbc0ef6
Hugo Pfeffer <hugo_13guimaraes@yahoo.com.br>
2020-06-13 23:59:09 -0300

    joinMember method

--- teste_discord.py
+++ teste_discord.py
@@ -1,26 +1,27 @@
 import discord
-import asyncio
 import os
+import asyncio
 from discord.ext import commands
+TOKEN = "NTk1Mzc4Mzk4MDcxNzUwNzU3.XuWFAg.s5NXpmLo-pYZWmAX5_5PeG_Xv4o"
+client = commands.Bot(command_prefix= '!')
 
-TOKEN = os.getenv("discord_token")
-client = commands.Bot(command_prefix="/")
-
-
-async def my_background_task():
-    print("teste1")
-    await client.wait_until_ready()
-    await client.change_presence(game=discord.Game(name="teste"))
-    print(1)
-
-
+# Bot tester
 @client.event
 async def on_ready():
-    print("Logged in as")
-    print(client.user.name)
-    print(client.user.id)
-    print("------")
+    print("Bot alive!")
+
+@client.command(name = "start")
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    await message.send('O jogo começou!')
+
+@client.command(name = "join", pass_context=True)
+async def joinMemberId(ctx): 
+    userId = ctx.author.id
+    userName = ctx.author.display_name
+    # await ctx.send("ID: {} \n Nome: {}".format(str(userId), str(userName)))
+    vagabonds = {userId:userName}
+    return vagabonds
 
 
-client.loop.create_task(my_background_task())
 client.run(TOKEN)
