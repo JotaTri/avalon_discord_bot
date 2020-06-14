c778ce643cd31f6b485b4379e8b48c64c7416fa6
João Menezes <jotatri@gmail.com>
2020-06-14 00:07:13 -0300

    added new comment

--- teste_discord.py
+++ teste_discord.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 TOKEN = os.getenv("discord_token")
 client = commands.Bot(command_prefix="/")
 
+# game(qty, id_discord, name)
 
 async def my_background_task():
     print("teste1")
@@ -20,6 +21,22 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print("------")
+
+
+@client.command(name='vote', pass_context=True)
+async def new_vote(ctx, arg):
+    yes_list = ['sim', 'yep', 'claro', 'sure', '1', 'um']
+    no_list = ['não', 'nao', 'no', 'nope']
+    # game_round = game.round()
+    print(ctx.author)
+    print(ctx.author.display_name)
+    if arg.lower() in yes_list:
+        await ctx.send('vote sim')
+    elif arg.lower() in no_list:
+        await ctx.send('vote não')
+    else:
+        await ctx.send('seu bosta')
+    # print(message)
 
 
 client.loop.create_task(my_background_task())
