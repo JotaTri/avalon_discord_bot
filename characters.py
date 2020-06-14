78b6ce901483b1a46e5c570e7a4742530501cdad
João Menezes <jotatri@gmail.com>
2020-06-14 00:02:37 -0300

    refactored characters class

--- characters.py
+++ characters.py
@@ -1,18 +1,18 @@
-import random
+# import random
 
 
-class characters:
-    def __init__(self, align, player_list, lead=False):
+class Character:
+    def __init__(self, align, player_info, leader=False):
         self.alignment = align
-        self.leader = lead
-        self.player_number = player_list[0]
-        self.player_id = player_list[1]
-        self.player_name = player_list[2]
-        self.team_vote = False
-        self.quest_vote = True
+        self.leader = leader
+        self.player_number = player_info['number']
+        self.player_id = player_info['dicord_id']
+        self.player_name = player_info['name']
+        self.team_vote = None
+        self.quest_vote = None
         self.assassin = False
         self.merlin = False
 
-    def assassinate(self, assassin, players_qty):
-        assassinated_player = random.sample(range(players_qty), 1)[0]
-        return assassinated_player
+    # def assassinate(self, assassin, players_qty):
+    #     assassinated_player = random.sample(range(players_qty), 1)
+    #     return assassinated_player
